c075d7c2dd28766ae2cb031f47c00d4483bd55f1
Dimon <varreact@gmail.com>
2018-06-07 02:11:46 -0400

    client

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import authReducers from './authReducers'
+import { reducer as reduxForm} from 'redux-from';
 
 export default combineReducers ({
-  auth: authReducers
+  auth: authReducers,
+  form: reduxForm
 })
