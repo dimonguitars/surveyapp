b00c9e01e5cbd832d0fdf4d3e29ccb98ae6e60ad
Dimon <slatqh@gmail.com>
2018-06-14 00:30:11 -0400

    front-end

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducers from "./authReducers";
+import surveysReducer from './surveysReducer';
 import { reducer as reduxForm } from "redux-form";
 
 export default combineReducers({
   auth: authReducers,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
